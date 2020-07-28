fd5bc8b719ea97ac51d55c0d242c4dded64d6806
Adam Kelly <adam@cthulahoops.org>
2020-07-28 13:33:11 +0100

    Add conversations table and cross references.

---
+++ priv/repo/migrations/20200728104429_add_conversations.exs
@@ -0,0 +1,23 @@
+defmodule Phail.Repo.Migrations.AddConversations do
+  use Ecto.Migration
+
+  def change do
+    create table(:conversations) do
+      add :subject, :text
+    end
+
+    alter table(:messages) do
+      add :message_id, :text
+      add :conversation_id, references(:conversations)
+    end
+
+    create table(:message_references, primary_key: false) do
+      add :message_id, references(:messages)
+      add :reference, :text
+    end
+
+    create unique_index(:message_references, [:message_id, :reference])
+    create index(:message_references, [:reference])
+    create index(:message_references, [:message_id])
+  end
+end

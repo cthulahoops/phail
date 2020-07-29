b3bdc8a5664b6fbc6296e448e16fa2dea309b09c
Adam Kelly <adam@cthulahoops.org>
2020-07-29 12:06:32 +0100

    Python scripts to load messages.

---
+++ loader/load_mail.py
@@ -0,0 +1,115 @@
+import sys
+import pathlib
+
+import psycopg2
+
+from message import Message
+
+from config_secret import (host, user, password, database)
+
+dbh = psycopg2.connect(host=host, user=user, password=password, database=database)
+
+def create_address(address):
+    cursor = dbh.cursor()
+    cursor.execute("insert into addresses (name, address) values (%s, %s) returning (id)", (
+        address['name'],
+        address['email']))
+    address = cursor.fetchone()
+    return address
+
+def get_address(address):
+    cursor = dbh.cursor()
+    cursor.execute("select id from addresses where name = %s and address = %s", (
+        address['name'],
+        address['email']))
+    return (cursor.fetchone() or create_address(address))[0]
+
+def insert_assoc(assoc_type, message_id, address_id):
+    table_name = f"message_{assoc_type}_address"
+    sql = f"insert into {table_name} (message_id, address_id) values (%s, %s)"
+    cursor = dbh.cursor()
+    cursor.execute(sql, (message_id, address_id))
+
+def insert_reference(message_id, reference):
+    with dbh.cursor() as cursor:
+        cursor.execute(
+            "insert into message_references (message_id, reference) values (%s, %s)", (
+                message_id,
+                reference))
+
+def get_referenced_conversation(message):
+    with dbh.cursor() as cursor:
+        cursor.execute("""select distinct messages.conversation_id
+            from messages
+            join message_references on messages.message_id = message_references.reference
+            join messages myself on myself.id = message_references.message_id
+            where myself.message_id = %s""", (message.message_id,))
+        conversation_ids = cursor.fetchall()
+        if len(conversation_ids) > 1:
+            print(conversation_ids)
+        if conversation_ids:
+            return conversation_ids[0]
+
+def get_referencing_conversation(message):
+    with dbh.cursor() as cursor:
+        cursor.execute("""select distinct messages.conversation_id
+            from messages
+            join message_references on messages.id = message_references.message_id
+            where message_references.reference = %s""", (message.message_id,))
+        conversation_ids = cursor.fetchall()
+        if len(conversation_ids) > 1:
+            print(conversation_ids)
+        if conversation_ids:
+            return conversation_ids[0]
+
+def get_new_conversation(message):
+    with dbh.cursor() as cursor:
+        cursor.execute("insert into conversations (subject) values (%s) returning (id)", (message.subject,))
+        return cursor.fetchone()
+
+def get_conversation(message):
+    return (
+        get_referenced_conversation(message)
+        or get_referencing_conversation(message)
+        or get_new_conversation(message))
+
+def insert_message(message):
+    from_addresses = list(map(get_address, message.addresses('from')))
+    to_addresses = list(map(get_address, message.addresses('to')))
+    cc_addresses = list(map(get_address, message.addresses('cc')))
+
+    cursor = dbh.cursor()
+    cursor.execute("insert into messages (subject, body, message_id, date) values (%s, %s, %s, %s) returning (id)", (
+        message.subject,
+        message.body,
+        message.message_id,
+        message.date,))
+    message_id = cursor.fetchone()[0]
+
+    for address_id in from_addresses:
+        insert_assoc("from", message_id, address_id)
+
+    for address_id in to_addresses:
+        insert_assoc("to", message_id, address_id)
+
+    for address_id in cc_addresses:
+        insert_assoc("cc", message_id, address_id)
+
+    for reference in message.references:
+        insert_reference(message_id, reference)
+
+    dbh.commit()
+
+    conversation_id = get_conversation(message)
+    with dbh.cursor() as cursor:
+        cursor.execute("update messages set conversation_id = %s where id = %s",
+            (conversation_id, message_id))
+
+
+def main():
+    for item in pathlib.Path(sys.argv[1]).iterdir():
+        message = Message(open(item, mode='rb'))
+        insert_message(message)
+
+if __name__ == '__main__':
+    main()
---
+++ loader/message.py
@@ -0,0 +1,197 @@
+import email
+import email.utils
+import email.header
+import datetime
+import base64
+import quopri
+import bleach
+
+CHARSET_ALIASES = {
+    "iso: western": "latin1",
+    "x-unknown": "latin1",
+    "unknown-8bit": "latin1",
+    "windows-874": "cp874",
+    "default": "utf-8",
+}
+
+
+def decode_header(header):
+    return "".join(
+        decode_charset(part, encoding)
+        for (part, encoding) in email.header.decode_header(header)
+    )
+
+class SimpleTz(datetime.tzinfo):
+    def __init__(self, offset):
+        self.offset = offset
+
+    def utcoffset(self, dt):
+        return datetime.timedelta(0, self.offset, 0)
+
+
+def parse_date(datestr):
+    date = email.utils.parsedate_tz(datestr)
+    tz = SimpleTz(date[-1] or 0)
+    date = date[:7] + (tz,)
+    return datetime.datetime(*date)
+
+
+class Message:
+    def __init__(self, fh, filename=None):
+        # if not filename:
+        #     filename = fh.name
+        self.text = fh.read()
+        self.filename = filename
+        self.mail = email.message_from_bytes(self.text)
+        self.subject = decode_header(self.mail.get("subject"))
+        self.message_id = self.mail.get("message-id")
+
+    @property
+    def is_unread(self):
+        return "/new/" in self.filename
+
+    @property
+    def date(self):
+        datestr = self.mail.get("date")
+        if not datestr:
+            return parse_date(self.mail.get("Received").split(";")[-1])
+        return parse_date(datestr)
+
+    def addresses(self, address_type):
+        headers = self.mail.get_all(address_type)
+        if headers:
+            return [
+                {"name": decode_header(name), "email": address}
+                for (name, address) in email.utils.getaddresses(headers)
+            ]
+        return []
+
+    @property
+    def references(self):
+        references = self.mail.get("references")
+        if references:
+            return references.split()
+        return []
+
+    @property
+    def body(self):
+        return content(self.mail)
+
+    def document(self):
+        try:
+            return {
+                "from": self.addresses("from"),
+                "to": self.addresses("to"),
+                "cc": self.addresses("cc"),
+                "subject": self.subject,
+                "message_id": self.message_id,
+                "date": self.date.isoformat(),
+                "filename": self.filename,
+                "unread": self.is_unread,
+                "body": self.body(),
+                # 		'original': base64.b64encode(message.text)
+            }
+        except:
+            print("Failure loading %r" % (self.filename,))
+            raise
+
+
+def htmlify(text):
+    return text.replace(">", "&gt;").replace("<", "&lt;").replace("\n", "<br/>")
+
+
+def decode(mail):
+    text = mail.get_payload(decode=True)
+    character_encoding = mail.get_charsets()[0]
+    return decode_charset(text, character_encoding)
+
+
+def decode_charset(text, charset):
+    if charset is None:
+        return text
+    charset = charset.lower().strip()
+    charset = CHARSET_ALIASES.get(charset, charset)
+    return text.decode(encoding=charset, errors="ignore")
+
+
+def decode_transfer(text, encoding):
+    if encoding:
+        encoding = encoding.lower().strip()
+
+    if encoding == "quoted-printable":
+        return quopri.decodestring(text)
+    if encoding == "base64":
+        return base64.b64decode(text)
+    if encoding in (None, "7bit", "8bit", "binary"):
+        return text
+    raise ValueError("Unsupported transfer encoding: %r" % (encoding,))
+
+
+alternative_order = {"text/plain": 1, "text/html": 2}
+
+
+def content(mail):
+    content_type = mail.get_content_type()
+    if content_type in ("multipart/mixed", "multipart/related"):
+        for part in mail.get_payload():
+            disposition = part.get("Content-Disposition")
+            if disposition and (
+                disposition.startswith("attachment") or disposition.startswith("inline")
+            ):
+                continue
+            return content(part)
+        return ""
+    if content_type in (
+        "multipart/alternative",
+        "multipart/signed",
+        "multipart/report",
+    ):
+        preferred = max(
+            mail.get_payload(),
+            key=lambda m: alternative_order.get(m.get_content_type(), 0),
+        )
+        return content(preferred)
+    if content_type == "text/plain":
+        return htmlify(decode(mail))
+    if content_type == "text/html":
+        html = decode(mail)
+        return bleach.clean(
+            html,
+            tags=[
+                "a",
+                "abbr",
+                "acronym",
+                "b",
+                "blockquote",
+                "code",
+                "em",
+                "i",
+                "li",
+                "ol",
+                "strong",
+                "ul",
+                "p",
+                "br",
+                "div",
+                "span",
+                "body",
+                "h1",
+                "h2",
+                "h3",
+                "table",
+                "td",
+                "tr",
+                "tbody",
+                "thead",
+                "img",
+            ],
+            attributes={
+                "a": ["href", "title"],
+                "abbr": ["title"],
+                "acronym": ["title"],
+                "img": ["alt"],
+                "*": ["style"],
+            },
+            styles=["color", "font-family", "background-color", "font-size"],
+        )
+    raise ValueError("Unsupported: %r" % (content_type,))

04f8ab0d8f17c53cbe31f6f4b0e4fe310cf99725
Adam Kelly <adam@cthulahoops.org>
2020-08-31 13:37:33 +0100

    Fix query parsing so that tests pass.

--- lib/phail/query.ex
+++ lib/phail/query.ex
@@ -9,13 +9,20 @@ defmodule Phail.Query do
       query_terms,
       %Phail.Query{},
       fn
-        {:label, label_name}, query -> %{query | labels: [label_name | query.labels]}
-        {:text, text}, query -> %{query | text_terms: [text | query.text_terms]}
+        {:label, label_name}, query -> %{query | labels: query.labels ++ [label_name]}
+        {:text, text}, query -> %{query | text_terms: query.text_terms ++ [text]}
       end
     )
   end
 
-  defp parser, do: sep_by(search_term(), spaces())
+  defp parser() do
+    between(
+      option(spaces()),
+      sep_by(search_term(), spaces()),
+      option(spaces())
+    )
+    |> eof
+  end
 
   defp search_term(previous \\ nil) do
     previous
@@ -24,7 +31,7 @@ defmodule Phail.Query do
 
   defp text_term(previous \\ nil) do
     previous
-    |> word
+    |> word_of(~r/[^ :]/)
     |> map(fn x -> {:text, x} end)
   end
 
@@ -42,17 +49,18 @@ defmodule Phail.Query do
     between(parser, char("\""), word_of(~r/[^"]/), char("\""))
   end
 
-
   def format_label(label_name) do
-    quoted_label_name = if String.match?(label_name, ~r/^\w*$/) do
-      label_name
-    else
-      "\"" <> label_name <> "\""
-    end
+    quoted_label_name =
+      if String.match?(label_name, ~r/^\w*$/) do
+        label_name
+      else
+        "\"" <> label_name <> "\""
+      end
+
     "label:" <> quoted_label_name
   end
 
   def format(query) do
-    Enum.join(Enum.map(query.labels, &format_label/1), " ") <> " " <> Enum.join(query.text_terms, " ")
+    Enum.join(Enum.map(query.labels, &format_label/1) ++ query.text_terms, " ")
   end
 end

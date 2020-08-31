b171750a68fdccdd581f74c24a412735fe2e6cce
Adam Kelly <adam@cthulahoops.org>
2020-08-31 13:36:53 +0100

    Changes to get tests to pass.

--- config/test.exs
+++ config/test.exs
@@ -1,17 +1,12 @@
 use Mix.Config
 
+import_config "test.secret.exs"
 # Configure your database
 #
 # The MIX_TEST_PARTITION environment variable can be used
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
-config :phail, Phail.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "phail_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
-
+#
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :phail, PhailWeb.Endpoint,

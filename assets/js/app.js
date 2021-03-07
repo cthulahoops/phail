1a4c90f9a55023b90e25d0eb0370b60a65e822ed
Adam Kelly <adam@cthulahoops.org>
2021-03-07 21:34:23 +0000

    Use events to separate Js and LiveView aspects of multi-select.

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,103 +17,173 @@ import {Socket} from "phoenix"
 import NProgress from "nprogress"
 import {LiveSocket} from "phoenix_live_view"
 
-let Hooks = {}
-Hooks.AddressSuggestion = {
-  updated() {
-    if (parseInt(this.el.attributes["phx-value-index"].value) === activeIndex) {
-      this.el.classList.add("active-suggestion");
-    }
-  },
-  mounted() {
-    if (parseInt(this.el.attributes["phx-value-index"].value) === activeIndex) {
-      this.el.classList.add("active-suggestion");
-    }
-    this.el.addEventListener("mouseover", event => {
-      setActiveIndex(parseInt(this.el.attributes["phx-value-index"].value));
-    })
-    this.el.addEventListener("click", event => {
-      this.pushEvent("add_to", {
-        id: this.el.attributes["phx-value-id"].value
-      })
-    })
-  }
-}
-
-let activeIndex = 0;
-const setActiveIndex = (newIndex) => {
-    const container = document.getElementById("to-address-container");
-    if (newIndex >= container.children.length) {
-        newIndex = container.children.length - 1;
-    } else if (newIndex < 0) {
-        newIndex = 0;
+class MultiInput extends HTMLElement {
+    constructor() {
+      super()
+      this._activeIndex = 0;
     }
 
-    if (activeIndex !== newIndex) {
-        if (activeIndex < container.children.length) {
-            container.children[activeIndex].classList.remove("active-suggestion");
-        }
-        container.children[newIndex].classList.add("active-suggestion");
-    }
-
-    activeIndex = newIndex;
-}
-
-Hooks.AddressInput = {
-    mounted() {
-        this.el.addEventListener("keydown", event => {
+    connectedCallback() {
+      this.addEventListener("keydown", () => {
             console.log(event.keyCode, event.key);
+            const input = event.originalTarget;
             switch (event.key) {
                 case "ArrowDown":
-                    setActiveIndex(activeIndex + 1);
+                    this.activeIndex += 1
                     break;
                 case "ArrowUp":
-                    setActiveIndex(activeIndex - 1);
+                    this.activeIndex -= 1
                     break;
                 case "Tab":
                 case "Enter":
-                    if (this.el.value === "") {
+                    event.preventDefault()
+                    if (input.value === "") {
                       return
                     }
-                    let container = document.getElementById("to-address-container");
-                    if (container === null) {
-                      this.pushEvent("add_to", {
-                        address: this.el.value
-                      })
+
+                    let detail;
+                    if (this.activeElement === null) {
+                      detail = { address: input.value }
                     } else {
-                      let activeElement = container.children[activeIndex];
-                      this.pushEvent("add_to", {
-                        id: activeElement.attributes["phx-value-id"].value
-                      })
+                      console.log("Active: ", this.activeElement, this.activeIndex);
+                      detail = { id: this.activeElement.suggestionId }
                     }
-                    this.el.value = "";
-                    event.preventDefault()
+
+                    this.dispatchEvent(new CustomEvent("suggestionSelect", {
+                      detail: detail
+                    }))
+                    input.value = "";
                     break;
                 case "Backspace":
-                    if (this.el.value === "") {
-                      const addresses = document.getElementById("to-input-div").children;
+                    if (input.value === "") {
+                      const addresses = this.children;
                       if (addresses.length > 1) {
                         const last = addresses[addresses.length - 2] // Very last element is the input box.
-                        this.pushEvent("remove_to_address", {
-                          id: last.querySelector("a.button").attributes["phx-value-id"].value
-                        })
+                        this.dispatchEvent(new CustomEvent("removeSelection", {
+                          detail: {
+                            id: last.querySelector("a.button").attributes["phx-value-id"].value
+                          }
+                        }))
                       }
                     } else {
                       return
                     }
+                    break
                 case "Escape":
-                    this.pushEvent("clear_suggestions", {})
+                    this.dispatchEvent(new CustomEvent("clearSuggestions"))
                     break
                 default:
                     return
             }
             event.preventDefault()
+      });
+      this.addEventListener("suggestionMouseOver", (event) => {
+        console.log(event.originalTarget, event.originalTarget.index);
+        this.activeIndex = event.originalTarget.index;
+      })
+
+      this.addEventListener("suggestionAdded", (event) => {
+        if (event.originalTarget.index == this.activeIndex) {
+          event.originalTarget.classList.add("active-suggestion");
+        }
+      })
+
+      this.querySelector('input').addEventListener("blur", event => {
+        setTimeout(() => {
+          console.log("Clearing suggestions.");
+          this.dispatchEvent(new CustomEvent("clearSuggestions"))
+        }, 100);
+      })
+    }
+
+    get activeIndex() {
+      return this._activeIndex
+    }
+
+    set activeIndex(newIndex) {
+      console.log("Set active: ", newIndex);
+      const container = this.querySelector(".autocomplete_suggestions")
+      if (newIndex >= container.children.length) {
+          newIndex = container.children.length - 1;
+      } else if (newIndex < 0) {
+          newIndex = 0;
+      }
+
+      if (this._activeIndex !== newIndex) {
+        if (this.activeIndex < container.children.length) {
+            container.children[this._activeIndex].classList.remove("active-suggestion");
+        }
+        container.children[newIndex].classList.add("active-suggestion");
+
+        this._activeIndex = newIndex;
+      }
+    }
+
+    get activeElement() {
+      let container = this.querySelector(".autocomplete_suggestions");
+      if (container !== null) {
+        return container.children[this.activeIndex];
+      }
+      else {
+        return null;
+      }
+    }
+}
+customElements.define('multi-input', MultiInput)
+
+class MultiInputSuggestion extends HTMLElement {
+  connectedCallback() {
+    console.log("Connected callback.");
+
+    this.addEventListener("mouseover", event => {
+      console.log("Mouse!");
+      this.dispatchEvent(new CustomEvent("suggestionMouseOver", {
+        bubbles: true
+        }))
+    })
+
+    this.addEventListener("click", event => {
+      this.dispatchEvent(new CustomEvent("suggestionSelect", {
+        bubbles: true,
+        detail: {
+          id: this.suggestionId
+        }
+      }))
+    })
+
+    this.dispatchEvent(new CustomEvent("suggestionAdded", {
+      bubbles: true,
+    }));
+  }
+
+  get index() {
+    console.log("This: ", this)
+    return parseInt(this.attributes["index"].value);
+  }
+
+  get suggestionId() {
+    return this.attributes["suggestion-id"].value;
+  }
+}
+
+customElements.define('multi-input-suggestion', MultiInputSuggestion)
+
+let Hooks = {}
+
+Hooks.AddressInput = {
+    mounted() {
+        this.el.addEventListener("suggestionSelect", event => {
+          this.pushEvent("add_to", event.detail);
         })
-        this.el.addEventListener("blur", event => {
-          setTimeout(() => {
-            console.log("Clearing suggestions.");
-            this.pushEvent("clear_suggestions");
-          }, 100);
+
+        this.el.addEventListener("clearSuggestions", event => {
+          this.pushEvent("clear_suggestions", {});
         })
+
+        this.el.addEventListener("removeSelection", event => {
+          this.pushEvent("remove_to_address", event.detail);
+        })
+
     }
 }
 

2d2395b1b5954cfca4b1a5bf11ea069317311b47
Adam Kelly <adam@cthulahoops.org>
2021-03-08 18:04:12 +0000

    Format code with standardjs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.scss"
+import '../css/app.scss'
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -12,138 +12,139 @@ import "../css/app.scss"
 //     import {Socket} from "phoenix"
 //     import socket from "./socket"
 //
-import "phoenix_html"
-import {Socket} from "phoenix"
-import NProgress from "nprogress"
-import {LiveSocket} from "phoenix_live_view"
+import 'phoenix_html'
+import { Socket } from 'phoenix'
+import NProgress from 'nprogress'
+import { LiveSocket } from 'phoenix_live_view'
+
+/* global HTMLElement, CustomEvent, customElements */
 
 class MultiInput extends HTMLElement {
-    constructor() {
-      super()
-      this._activeIndex = 0;
-    }
+  constructor () {
+    super()
+    this._activeIndex = 0
+  }
 
-    connectedCallback() {
-      this.addEventListener("keydown", () => {
-            console.log(event.keyCode, event.key);
-            const input = event.originalTarget;
-            switch (event.key) {
-                case "ArrowDown":
-                    this.activeIndex += 1
-                    break;
-                case "ArrowUp":
-                    this.activeIndex -= 1
-                    break;
-                case "Tab":
-                case "Enter":
-                    event.preventDefault()
-                    if (input.value === "") {
-                      return
-                    }
+  connectedCallback () {
+    this.addEventListener('keydown', (event) => {
+      console.log(event.keyCode, event.key)
+      const input = event.originalTarget
+      let detail
+      switch (event.key) {
+        case 'ArrowDown':
+          this.activeIndex += 1
+          break
+        case 'ArrowUp':
+          this.activeIndex -= 1
+          break
+        case 'Tab':
+        case 'Enter':
+          event.preventDefault()
+          if (input.value === '') {
+            return
+          }
 
-                    let detail;
-                    if (this.activeElement === null) {
-                      detail = { address: input.value }
-                    } else {
-                      console.log("Active: ", this.activeElement, this.activeIndex);
-                      detail = { id: this.activeElement.suggestionId }
-                    }
+          if (this.activeElement === null) {
+            detail = { address: input.value }
+          } else {
+            console.log('Active: ', this.activeElement, this.activeIndex)
+            detail = { id: this.activeElement.suggestionId }
+          }
 
-                    this.dispatchEvent(new CustomEvent("suggestionSelect", {
-                      detail: detail
-                    }))
-                    input.value = "";
-                    break;
-                case "Backspace":
-                    if (input.value === "") {
-                      const addresses = this.children;
-                      if (addresses.length > 1) {
-                        const last = addresses[addresses.length - 2] // Very last element is the input box.
-                        this.dispatchEvent(new CustomEvent("removeSelection", {
-                          detail: {
-                            id: last.querySelector("a.button").attributes["phx-value-id"].value
-                          }
-                        }))
-                      }
-                    } else {
-                      return
-                    }
-                    break
-                case "Escape":
-                    this.dispatchEvent(new CustomEvent("clearSuggestions"))
-                    break
-                default:
-                    return
+          this.dispatchEvent(new CustomEvent('suggestionSelect', {
+            detail: detail
+          }))
+          input.value = ''
+          break
+        case 'Backspace':
+          if (input.value === '') {
+            const addresses = this.children
+            if (addresses.length > 1) {
+              const last = addresses[addresses.length - 2] // Very last element is the input box.
+              this.dispatchEvent(new CustomEvent('removeSelection', {
+                detail: {
+                  id: last.querySelector('a.button').attributes['phx-value-id'].value
+                }
+              }))
             }
-            event.preventDefault()
-      });
-      this.addEventListener("suggestionMouseOver", (event) => {
-        console.log(event.originalTarget, event.originalTarget.index);
-        this.activeIndex = event.originalTarget.index;
-      })
+          } else {
+            return
+          }
+          break
+        case 'Escape':
+          this.dispatchEvent(new CustomEvent('clearSuggestions'))
+          break
+        default:
+          return
+      }
+      event.preventDefault()
+    })
+    this.addEventListener('suggestionMouseOver', (event) => {
+      console.log(event.originalTarget, event.originalTarget.index)
+      this.activeIndex = event.originalTarget.index
+    })
 
-      this.addEventListener("suggestionAdded", (event) => {
-        if (event.originalTarget.index == this.activeIndex) {
-          event.originalTarget.classList.add("active-suggestion");
-        }
-      })
+    this.addEventListener('suggestionAdded', (event) => {
+      if (event.originalTarget.index === this.activeIndex) {
+        event.originalTarget.classList.add('active-suggestion')
+      }
+    })
 
-      this.querySelector('input').addEventListener("blur", event => {
-        setTimeout(() => {
-          console.log("Clearing suggestions.");
-          this.dispatchEvent(new CustomEvent("clearSuggestions"))
-        }, 100);
-      })
+    this.querySelector('input').addEventListener('blur', event => {
+      setTimeout(() => {
+        console.log('Clearing suggestions.')
+        this.dispatchEvent(new CustomEvent('clearSuggestions'))
+      }, 100)
+    })
+  }
+
+  get activeIndex () {
+    return this._activeIndex
+  }
+
+  set activeIndex (newIndex) {
+    console.log('Set active: ', newIndex)
+    const container = this.querySelector('.autocomplete_suggestions')
+    if (newIndex >= container.children.length) {
+      newIndex = container.children.length - 1
+    } else if (newIndex < 0) {
+      newIndex = 0
     }
 
-    get activeIndex() {
-      return this._activeIndex
+    if (this._activeIndex !== newIndex) {
+      if (this.activeIndex < container.children.length) {
+        container.children[this._activeIndex].classList.remove('active-suggestion')
+      }
+      container.children[newIndex].classList.add('active-suggestion')
+
+      this._activeIndex = newIndex
     }
+  }
 
-    set activeIndex(newIndex) {
-      console.log("Set active: ", newIndex);
-      const container = this.querySelector(".autocomplete_suggestions")
-      if (newIndex >= container.children.length) {
-          newIndex = container.children.length - 1;
-      } else if (newIndex < 0) {
-          newIndex = 0;
-      }
-
-      if (this._activeIndex !== newIndex) {
-        if (this.activeIndex < container.children.length) {
-            container.children[this._activeIndex].classList.remove("active-suggestion");
-        }
-        container.children[newIndex].classList.add("active-suggestion");
-
-        this._activeIndex = newIndex;
-      }
+  get activeElement () {
+    const container = this.querySelector('.autocomplete_suggestions')
+    if (container !== null) {
+      return container.children[this.activeIndex]
+    } else {
+      return null
     }
-
-    get activeElement() {
-      let container = this.querySelector(".autocomplete_suggestions");
-      if (container !== null) {
-        return container.children[this.activeIndex];
-      }
-      else {
-        return null;
-      }
-    }
+  }
 }
 customElements.define('multi-input', MultiInput)
 
 class MultiInputSuggestion extends HTMLElement {
-  connectedCallback() {
-    console.log("Connected callback.");
+  connectedCallback () {
+    console.log('Connected callback.')
 
-    this.addEventListener("mouseover", event => {
-      console.log("Mouse!");
-      this.dispatchEvent(new CustomEvent("suggestionMouseOver", {
+    this.addEventListener('mouseover', event => {
+      console.log('Mouse!')
+      this.dispatchEvent(new CustomEvent('suggestionMouseOver', {
         bubbles: true
-        }))
+      }))
     })
 
-    this.addEventListener("click", event => {
-      this.dispatchEvent(new CustomEvent("suggestionSelect", {
+    this.addEventListener('click', event => {
+      this.dispatchEvent(new CustomEvent('suggestionSelect', {
         bubbles: true,
         detail: {
           id: this.suggestionId
@@ -151,49 +152,47 @@ class MultiInputSuggestion extends HTMLElement {
       }))
     })
 
-    this.dispatchEvent(new CustomEvent("suggestionAdded", {
-      bubbles: true,
-    }));
+    this.dispatchEvent(new CustomEvent('suggestionAdded', {
+      bubbles: true
+    }))
   }
 
-  get index() {
-    console.log("This: ", this)
-    return parseInt(this.attributes["index"].value);
+  get index () {
+    console.log('This: ', this)
+    return parseInt(this.attributes.index.value)
   }
 
-  get suggestionId() {
-    return this.attributes["suggestion-id"].value;
+  get suggestionId () {
+    return this.attributes['suggestion-id'].value
   }
 }
 
 customElements.define('multi-input-suggestion', MultiInputSuggestion)
 
-let Hooks = {}
+const Hooks = {}
 
 Hooks.AddressInput = {
-    mounted() {
-        this.el.addEventListener("suggestionSelect", event => {
-          this.pushEvent("add_to", event.detail);
-        })
+  mounted () {
+    this.el.addEventListener('suggestionSelect', event => {
+      this.pushEvent('add_to', event.detail)
+    })
 
-        this.el.addEventListener("clearSuggestions", event => {
-          this.pushEvent("clear_suggestions", {});
-        })
+    this.el.addEventListener('clearSuggestions', event => {
+      this.pushEvent('clear_suggestions', {})
+    })
 
-        this.el.addEventListener("removeSelection", event => {
-          this.pushEvent("remove_to_address", event.detail);
-        })
-
-    }
+    this.el.addEventListener('removeSelection', event => {
+      this.pushEvent('remove_to_address', event.detail)
+    })
+  }
 }
 
-
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}, hooks: Hooks})
+const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute('content')
+const liveSocket = new LiveSocket('/live', Socket, { params: { _csrf_token: csrfToken }, hooks: Hooks })
 
 // Show progress bar on live navigation and form submits
-window.addEventListener("phx:page-loading-start", info => NProgress.start())
-window.addEventListener("phx:page-loading-stop", info => NProgress.done())
+window.addEventListener('phx:page-loading-start', info => NProgress.start())
+window.addEventListener('phx:page-loading-stop', info => NProgress.done())
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()

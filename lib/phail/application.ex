27ece8c567f9b3349da002f770a5e3fc2962cc4c
Adam Kelly <adam@cthulahoops.org>
2021-05-05 15:09:01 +0100

    Reformatting supervisor changes.

--- lib/phail/application.ex
+++ lib/phail/application.ex
@@ -6,25 +6,27 @@ defmodule Phail.Application do
   use Application
 
   def start(_type, _args) do
-    children = [
-      # Start the Ecto repository
-      Phail.Repo,
-      # Start the Telemetry supervisor
-      PhailWeb.Telemetry,
-      # Start the PubSub system
-      {Phoenix.PubSub, name: Phail.PubSub},
-      # Start the Endpoint (http/https)
-      PhailWeb.Endpoint,
-      # Start a worker by calling: Phail.Worker.start_link(arg)
-      # {Phail.Worker, arg}
-      #
-      {Registry, keys: :unique, name: Phail.Fetchmail.Registry},
-      {DynamicSupervisor, name: Phail.Fetchmail.Supervisor, strategy: :one_for_one},
-    ] ++ if Application.get_env(:phail, :start_account_pollers) do
-      [Phail.Fetchmail.Poller]
-    else
-      []
-    end
+    children =
+      [
+        # Start the Ecto repository
+        Phail.Repo,
+        # Start the Telemetry supervisor
+        PhailWeb.Telemetry,
+        # Start the PubSub system
+        {Phoenix.PubSub, name: Phail.PubSub},
+        # Start the Endpoint (http/https)
+        PhailWeb.Endpoint,
+        # Start a worker by calling: Phail.Worker.start_link(arg)
+        # {Phail.Worker, arg}
+        #
+        {Registry, keys: :unique, name: Phail.Fetchmail.Registry},
+        {DynamicSupervisor, name: Phail.Fetchmail.Supervisor, strategy: :one_for_one}
+      ] ++
+        if Application.get_env(:phail, :start_account_pollers) do
+          [Phail.Fetchmail.Poller]
+        else
+          []
+        end
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
